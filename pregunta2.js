d54e42a5a8efdf2e6d7063819161f81c0ca1f0ce
jossep <foxito_185@hotmail.com>
2021-01-14 10:31:50 -0500

    tercer conmit

---
+++ pregunta2.js
@@ -0,0 +1,53 @@
+function operadorbasico (){
+    total=100;
+    subtotal=total/1.18;
+    igv=subtotal*0.18;
+    console.log("el total es "+total);
+    console.log("el subtotal es "+subtotal);
+    console.log("el igv es "+igv);
+    }
+function concionalif (){
+        nota=14;
+        nota2=12;
+        nota3=10;
+        prom=(nota+nota2+nota3)/3;
+        if(prom>10){
+            console.log("nota 1 "+nota);
+            console.log("nota 2 "+nota2);
+            console.log("nota 3 "+nota3);
+            console.log("el promedio es "+prom);
+            console.log("aprovo el curso");
+        }
+        else{
+            console.log("nota 1 "+nota);
+            console.log("nota 2 "+nota2);
+            console.log("nota 3 "+nota3);
+            console.log("el promedio es "+prom);
+            console.log("desaprovo el curso");
+
+        }
+         }
+         function nume (){
+            num=12;
+            num2=10;
+            res =num+num2;
+            res2=num-num2;
+            res3=num*num2;
+            res4=num/num2;
+            res5=num%num2;
+            console.log("la suma es "+res)
+            console.log("la resta es "+res2)
+            console.log("la multiplicacion es "+res3)
+            console.log("la divicion es "+res4)
+            console.log("el resto es "+res5)
+             }
+         
+
+    
+    
+
+
+
+
+
+     
